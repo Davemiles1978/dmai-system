645b91da81f64cc38bf33114ad1845dde233247c
Davemiles1978 <83094232+Davemiles1978@users.noreply.github.com>
2026-02-28 11:42:11 +0300

    Update .gitignore to exclude backup and temporary files

--- checkpoints/best_versions/core/__init__.py
+++
@@ -1,9 +0,0 @@
-# EVOLVE-BLOCK-START - ENTIRE FILE CAN EVOLVE
-    # Evolution improvement at generation 21
-# This file is part of the AI Evolution System
-# All code below can be modified and improved through evolution
-
-"""Core crewAI components and interfaces."""
-
-
-# EVOLVE-BLOCK-END
